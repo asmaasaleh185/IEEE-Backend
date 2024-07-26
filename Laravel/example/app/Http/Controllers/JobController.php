e303433013ecdbe07d2c0c35c05b0f45ad3eac1d
asmaasaleh185 <asmaasaleh185@gmail.com>
2024-07-27 00:31:14 +0300

    Task-23

--- Laravel/example/app/Http/Controllers/JobController.php
+++ Laravel/example/app/Http/Controllers/JobController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
+use App\Models\User;
+use App\Mail\JobPosted;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -24,11 +29,15 @@ class JobController extends Controller
             'salary' => ['required'],
         ]);
 
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
+
+        Mail::to($job->employer->user)->queue(
+            new JobPosted($job)
+        );
 
         return redirect('/jobs');
     }
@@ -39,10 +48,14 @@ class JobController extends Controller
     }
     public function edit(Job $job)
     {
+        // Gate::authorize('edit-job', $job);
+
         return view('jobs.edit', ['job' => $job]);
     }
     public function update(Job $job)
     {
+        Gate::authorize('edit-job', $job);
+
         // validate
         request()->validate([
             'title' => ['required', 'min:3'],
@@ -61,6 +74,8 @@ class JobController extends Controller
     }
     public function destroy(Job $job)
     {
+        Gate::authorize('edit-job', $job);
+
         //delete
         $job->delete();
 
